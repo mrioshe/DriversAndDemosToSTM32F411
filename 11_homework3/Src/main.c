773ced6a94b8d664bfb403e27d4e8b69d9028d60
mrioshe <80715697+diperezp@users.noreply.github.com>
2023-11-02 11:45:07 -0500

    inicio main tarea 3

--- 11_homework3/Src/main.c
+++ 11_homework3/Src/main.c
@@ -39,6 +39,9 @@ char bufferData[64]={0};
 //Elementos para la conversion ADC
 ADC_Config_t potenciometro ={0};
 
+void segment_configuration_units(uint8_t number);
+void segment_configuration_decs(ADC_Config_t *adcConfig);
+
 void initSys(void);
 
 int main() {
@@ -91,6 +94,147 @@ void initSys(void) {
 	userLed.pinConfig.GPIO_PinOutputType = GPIO_OTYPE_PUSHPULL;
 	userLed.pinConfig.GPIO_PinOutputSpeed = GPIO_OSPEEDR_MEDIUM;
 	userLed.pinConfig.GPIO_PinPuPdControl = GPIO_PUPDR_NOTHING;
+
+	/*Configuración:*/
+
+	//Pin para conectar led de estado
+
+	stateled.pGPIOx							= GPIOA;
+	stateled.pinConfig.GPIO_PinNumber		= PIN_5;
+	stateled.pinConfig.GPIO_PinMode			= GPIO_MODE_OUT;
+	stateled.pinConfig.GPIO_PinOutputType	= GPIO_OTYPE_PUSHPULL;
+	stateled.pinConfig.GPIO_PinOutputSpeed	= GPIO_OSPEEDR_MEDIUM;
+	stateled.pinConfig.GPIO_PinPuPdControl	= GPIO_PUPDR_NOTHING;
+
+
+	//Pin para conectar led que indica el sentido de rotación
+
+	direction.pGPIOx							= GPIOC;
+	direction.pinConfig.GPIO_PinNumber			= PIN_1;
+	direction.pinConfig.GPIO_PinMode			= GPIO_MODE_OUT;
+	direction.pinConfig.GPIO_PinOutputType		= GPIO_OTYPE_PUSHPULL;
+	direction.pinConfig.GPIO_PinOutputSpeed		= GPIO_OSPEEDR_MEDIUM;
+	direction.pinConfig.GPIO_PinPuPdControl		= GPIO_PUPDR_NOTHING;
+
+	//Pin de entrada del botón del encoder
+
+	sw.pGPIOx							= GPIOC;
+	sw.pinConfig.GPIO_PinNumber			= PIN_0;
+	sw.pinConfig.GPIO_PinMode			= GPIO_MODE_IN;
+	sw.pinConfig.GPIO_PinOutputType		= GPIO_OTYPE_PUSHPULL;
+	sw.pinConfig.GPIO_PinOutputSpeed	= GPIO_OSPEEDR_MEDIUM;
+	sw.pinConfig.GPIO_PinPuPdControl	= GPIO_PUPDR_NOTHING;
+
+	//Pin de entrada del data del encoder
+
+	dt.pGPIOx							= GPIOA;
+	dt.pinConfig.GPIO_PinNumber			= PIN_10;
+	dt.pinConfig.GPIO_PinMode			= GPIO_MODE_IN;
+	dt.pinConfig.GPIO_PinOutputType		= GPIO_OTYPE_PUSHPULL;
+	dt.pinConfig.GPIO_PinOutputSpeed	= GPIO_OSPEEDR_MEDIUM;
+	dt.pinConfig.GPIO_PinPuPdControl	= GPIO_PUPDR_NOTHING;
+
+
+	//Pin de entrada del clock del reloj del encoder
+
+	clk.pGPIOx							= GPIOC;
+	clk.pinConfig.GPIO_PinNumber		= PIN_9;
+	clk.pinConfig.GPIO_PinMode			= GPIO_MODE_IN;
+	clk.pinConfig.GPIO_PinOutputType	= GPIO_OTYPE_PUSHPULL;
+	clk.pinConfig.GPIO_PinOutputSpeed	= GPIO_OSPEEDR_MEDIUM;
+	clk.pinConfig.GPIO_PinPuPdControl	= GPIO_PUPDR_NOTHING;
+
+	/*PINES DEL 7 SEGMENTOS:*/
+
+	//segmento A
+
+	leds_7segment[0].pGPIOx							= GPIOD;
+	leds_7segment[0].pinConfig.GPIO_PinNumber		= PIN_2;
+	leds_7segment[0].pinConfig.GPIO_PinMode			= GPIO_MODE_OUT;
+	leds_7segment[0].pinConfig.GPIO_PinOutputSpeed	= GPIO_OSPEEDR_MEDIUM;
+
+	//segmento B
+
+	leds_7segment[1].pGPIOx							= GPIOA;
+	leds_7segment[1].pinConfig.GPIO_PinNumber		= PIN_1;
+	leds_7segment[1].pinConfig.GPIO_PinMode			= GPIO_MODE_OUT;
+	leds_7segment[1].pinConfig.GPIO_PinOutputSpeed	= GPIO_OSPEEDR_MEDIUM;
+
+	//segmento C
+
+	leds_7segment[2].pGPIOx							= GPIOB;
+	leds_7segment[2].pinConfig.GPIO_PinNumber		= PIN_9;
+	leds_7segment[2].pinConfig.GPIO_PinMode			= GPIO_MODE_OUT;
+	leds_7segment[2].pinConfig.GPIO_PinOutputSpeed	= GPIO_OSPEEDR_MEDIUM;
+
+	//segmento D
+
+	leds_7segment[3].pGPIOx							= GPIOB;
+	leds_7segment[3].pinConfig.GPIO_PinNumber		= PIN_6;
+	leds_7segment[3].pinConfig.GPIO_PinMode			= GPIO_MODE_OUT;
+	leds_7segment[3].pinConfig.GPIO_PinOutputSpeed	= GPIO_OSPEEDR_MEDIUM;
+
+	//segmento E
+
+	leds_7segment[4].pGPIOx							= GPIOB;
+	leds_7segment[4].pinConfig.GPIO_PinNumber		= PIN_8;
+	leds_7segment[4].pinConfig.GPIO_PinMode			= GPIO_MODE_OUT;
+	leds_7segment[4].pinConfig.GPIO_PinOutputSpeed	= GPIO_OSPEEDR_MEDIUM;
+
+
+	//segmento F
+
+	leds_7segment[5].pGPIOx							= GPIOA;
+	leds_7segment[5].pinConfig.GPIO_PinNumber		= PIN_4;
+	leds_7segment[5].pinConfig.GPIO_PinMode			= GPIO_MODE_OUT;
+	leds_7segment[5].pinConfig.GPIO_PinOutputSpeed	= GPIO_OSPEEDR_MEDIUM;
+
+	//segmento G
+
+	leds_7segment[6].pGPIOx							= GPIOA;
+	leds_7segment[6].pinConfig.GPIO_PinNumber		= PIN_9;
+	leds_7segment[6].pinConfig.GPIO_PinMode			= GPIO_MODE_OUT;
+	leds_7segment[6].pinConfig.GPIO_PinOutputSpeed	= GPIO_OSPEEDR_MEDIUM;
+
+	//segmento DP
+
+	leds_7segment[7].pGPIOx							= GPIOC;
+	leds_7segment[7].pinConfig.GPIO_PinNumber		= PIN_8;
+	leds_7segment[7].pinConfig.GPIO_PinMode			= GPIO_MODE_OUT;
+	leds_7segment[7].pinConfig.GPIO_PinOutputSpeed	= GPIO_OSPEEDR_MEDIUM;
+
+
+	// pin que alimenta el primer led
+
+	vcc1_7seg.pGPIOx							= GPIOC;
+	vcc1_7seg.pinConfig.GPIO_PinNumber			= PIN_6;
+	vcc1_7seg.pinConfig.GPIO_PinMode			= GPIO_MODE_OUT;
+	vcc1_7seg.pinConfig.GPIO_PinOutputSpeed		= GPIO_OSPEEDR_MEDIUM;
+
+
+	// pin que alimenta el segundo led
+
+	vcc2_7seg.pGPIOx							= GPIOC;
+	vcc2_7seg.pinConfig.GPIO_PinNumber			= PIN_11;
+	vcc2_7seg.pinConfig.GPIO_PinMode			= GPIO_MODE_OUT;
+	vcc2_7seg.pinConfig.GPIO_PinOutputSpeed		= GPIO_OSPEEDR_MEDIUM;
+
+
+	// Configuramos el timer de 32 bits para el blinky
+
+    blinkTimer.pTIMx								=TIM5;
+	blinkTimer.TIMx_Config.TIMx_Prescaler			=16000; //Genera incrementos de 1 ms
+	blinkTimer.TIMx_Config.TIMx_Period				=250;   //DE la mano con el prescaler, se toma el periodo en ms
+	blinkTimer.TIMx_Config.TIMx_mode				=TIMER_UP_COUNTER;
+	blinkTimer.TIMx_Config.TIMx_InterruptEnable		=TIMER_INT_ENABLE;
+
+	// Configuramos el timer de 8 bits para obtener la frecuencia de switcheo de los dos canales del 7segmentos
+
+    frecTimer.pTIMx									=TIM10;
+    frecTimer.TIMx_Config.TIMx_Prescaler			=16000; //Genera incrementos de 1 ms
+    frecTimer.TIMx_Config.TIMx_Period				=10;   //DE la mano con el prescaler, se toma el periodo en ms
+    frecTimer.TIMx_Config.TIMx_mode					=TIMER_UP_COUNTER;
+    frecTimer.TIMx_Config.TIMx_InterruptEnable		=TIMER_INT_ENABLE;
 
 	gpio_Config(&userLed);
 	gpio_WritePin(&userLed,SET);
@@ -146,6 +290,8 @@ void initSys(void) {
 
 }
 
+
+
 void Timer2_Callback(void) {
 	gpio_TooglePin(&userLed);
 	sendMsg = 1;
@@ -157,6 +303,129 @@ void usart2_RxCallback(void) {
 
 void adc_CompleteCallback(void) {
 	potenciometro.adcData = adc_GetValue();
+}
+
+void segment_configuration_decs(ADC_Config_t *adcConfig){
+
+	switch(adcConfig->channel){
+
+	case RESOLUTION_12_BIT:
+
+		break;
+	default:
+		__NOP();
+
+	}
+}
+
+
+void segment_configuration_units(uint8_t number){
+
+	switch(number){
+
+		case 0:
+
+			gpio_WritePin(&leds_7segment[0],!SET);
+			gpio_WritePin(&leds_7segment[1],!SET);
+			gpio_WritePin(&leds_7segment[2],!SET);
+			gpio_WritePin(&leds_7segment[3],!SET);
+			gpio_WritePin(&leds_7segment[4],!SET);
+			gpio_WritePin(&leds_7segment[5],!SET);
+			gpio_WritePin(&leds_7segment[6],!RESET);
+			break;
+
+		case 1:
+			gpio_WritePin(&leds_7segment[0],!RESET);
+			gpio_WritePin(&leds_7segment[1],!SET);
+			gpio_WritePin(&leds_7segment[2],!SET);
+			gpio_WritePin(&leds_7segment[3],!RESET);
+			gpio_WritePin(&leds_7segment[4],!RESET);
+			gpio_WritePin(&leds_7segment[5],!RESET);
+			gpio_WritePin(&leds_7segment[6],!RESET);
+			break;
+
+		case 2:
+			gpio_WritePin(&leds_7segment[0],!SET);
+			gpio_WritePin(&leds_7segment[1],!SET);
+			gpio_WritePin(&leds_7segment[2],!RESET);
+			gpio_WritePin(&leds_7segment[3],!SET);
+			gpio_WritePin(&leds_7segment[4],!SET);
+			gpio_WritePin(&leds_7segment[5],!RESET);
+			gpio_WritePin(&leds_7segment[6],!SET);
+			break;
+
+		case 3:
+			gpio_WritePin(&leds_7segment[0],!SET);
+			gpio_WritePin(&leds_7segment[1],!SET);
+			gpio_WritePin(&leds_7segment[2],!SET);
+			gpio_WritePin(&leds_7segment[3],!SET);
+			gpio_WritePin(&leds_7segment[4],!RESET);
+			gpio_WritePin(&leds_7segment[5],!RESET);
+			gpio_WritePin(&leds_7segment[6],!SET);
+			break;
+
+		case 4:
+			gpio_WritePin(&leds_7segment[0],!RESET);
+			gpio_WritePin(&leds_7segment[1],!SET);
+			gpio_WritePin(&leds_7segment[2],!SET);
+			gpio_WritePin(&leds_7segment[3],!RESET);
+			gpio_WritePin(&leds_7segment[4],!RESET);
+			gpio_WritePin(&leds_7segment[5],!SET);
+			gpio_WritePin(&leds_7segment[6],!SET);
+			break;
+
+		case 5:
+			gpio_WritePin(&leds_7segment[0],!SET);
+			gpio_WritePin(&leds_7segment[1],!RESET);
+			gpio_WritePin(&leds_7segment[2],!SET);
+			gpio_WritePin(&leds_7segment[3],!SET);
+			gpio_WritePin(&leds_7segment[4],!RESET);
+			gpio_WritePin(&leds_7segment[5],!SET);
+			gpio_WritePin(&leds_7segment[6],!SET);
+			break;
+
+		case 6:
+			gpio_WritePin(&leds_7segment[0],!SET);
+			gpio_WritePin(&leds_7segment[1],!RESET);
+			gpio_WritePin(&leds_7segment[2],!SET);
+			gpio_WritePin(&leds_7segment[3],!SET);
+			gpio_WritePin(&leds_7segment[4],!SET);
+			gpio_WritePin(&leds_7segment[5],!SET);
+			gpio_WritePin(&leds_7segment[6],!SET);
+			break;
+
+		case 7:
+			gpio_WritePin(&leds_7segment[0],!SET);
+			gpio_WritePin(&leds_7segment[1],!SET);
+			gpio_WritePin(&leds_7segment[2],!SET);
+			gpio_WritePin(&leds_7segment[3],!RESET);
+			gpio_WritePin(&leds_7segment[4],!RESET);
+			gpio_WritePin(&leds_7segment[5],!RESET);
+			gpio_WritePin(&leds_7segment[6],!RESET);
+			break;
+
+		case 8:
+			gpio_WritePin(&leds_7segment[0],!SET);
+			gpio_WritePin(&leds_7segment[1],!SET);
+			gpio_WritePin(&leds_7segment[2],!SET);
+			gpio_WritePin(&leds_7segment[3],!SET);
+			gpio_WritePin(&leds_7segment[4],!SET);
+			gpio_WritePin(&leds_7segment[5],!SET);
+			gpio_WritePin(&leds_7segment[6],!SET);
+			break;
+
+
+		case 9:
+			gpio_WritePin(&leds_7segment[0],!SET);
+			gpio_WritePin(&leds_7segment[1],!SET);
+			gpio_WritePin(&leds_7segment[2],!SET);
+			gpio_WritePin(&leds_7segment[3],!SET);
+			gpio_WritePin(&leds_7segment[4],!RESET);
+			gpio_WritePin(&leds_7segment[5],!SET);
+			gpio_WritePin(&leds_7segment[6],!SET);
+			break;
+
+	}
 }
 
 
